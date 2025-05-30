78ba8e25373478c572c09f42f5e193027d0e4831
MatthewButtitta <matthewbuttitta@gmail.com>
2025-05-29 19:56:27 -0500

    entities updated

--- final/src/main/java/com/example/demo/entities/User.java
+++ final/src/main/java/com/example/demo/entities/User.java
@@ -30,31 +30,35 @@ public class User {
     @Column(nullable = false)
     private Role role;
 
+    @Column()
+    private String profilePicture;
+
     @Column(nullable = false)
     private Timestamp createdAt;
 
     @OneToMany(mappedBy = "listingAgent", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<Property> propertiesListed;
 
-//    @OneToMany
-//    private List<Property> propertiesFavorited;
-//
-//    @OneToMany
-//    private List<Message> messagesSent;
-//
-//    @OneToMany
-//    private List<Message> messagesReceived;
+    @OneToMany
+    private List<Property> propertiesFavorited;
+
+    @OneToMany
+    private List<Message> messages;
+
 
     public User() {}
 
     public User(String firstName, String lastName, String email,
-                String password, Role role) {
-        setFirstName(firstName);
-        setLastName(lastName);
-        setEmail(email);
-        setPassword(password);
-        setRole(role);
-        setCreatedAt();
+                String password, Role role, String profilePicture,
+                Timestamp createdAt, List<Property> propertiesListed) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.profilePicture = profilePicture;
+        this.createdAt = createdAt;
+        this.propertiesListed = propertiesListed;
     }
 
     public Long getId() {
@@ -105,6 +109,14 @@ public class User {
         this.role = role;
     }
 
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
     public Timestamp getCreatedAt() {
         return createdAt;
     }
@@ -122,27 +134,43 @@ public class User {
         this.propertiesListed = propertiesListed;
     }
 
-//    public List<Property> getPropertiesFavorited() {
-//        return propertiesFavorited;
-//    }
-//
-//    public void setPropertiesFavorited(List<Property> propertiesFavorited) {
-//        this.propertiesFavorited = propertiesFavorited;
-//    }
-//
-//    public List<Message> getMessagesSent() {
-//        return messagesSent;
-//    }
-//
-//    public void setMessagesSent(List<Message> messagesSent) {
-//        this.messagesSent = messagesSent;
-//    }
-//
-//    public List<Message> getMessagesReceived() {
-//        return messagesReceived;
-//    }
-//
-//    public void setMessagesReceived(List<Message> messagesReceived) {
-//        this.messagesReceived = messagesReceived;
-//    }
+    public List<Property> getPropertiesFavorited() {
+        return propertiesFavorited;
+    }
+
+    public void setPropertiesFavorited(List<Property> propertiesFavorited) {
+        this.propertiesFavorited = propertiesFavorited;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public void addToPropertiesListed(Property property){
+        this.propertiesListed.add(property);
+        property.setListingAgent(this);
+    }
+
+    public void removePropertyListed(Property property){
+        this.propertiesListed.remove(property);
+    }
+
+    public void addToPropertiesFavorited(Property property){
+        this.propertiesFavorited.add(property);
+        property.addToUsersFavorited(this);
+    }
+
+    public void addToMessages(Message message){
+        this.messages.add(message);
+    }
+
+    public void removeMessage(Message message){
+        this.messages.remove(message);
+    }
+
+
 }
