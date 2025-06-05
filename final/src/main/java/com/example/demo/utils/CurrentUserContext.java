3f6ccbb682e174e979a1d45dee0e89e52ddc6131
jjdubski <101855602+jjdubski@users.noreply.github.com>
2025-06-05 17:25:21 -0500

    update

--- final/src/main/java/com/example/demo/utils/CurrentUserContext.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.utils;
-
-import com.example.demo.entities.User;
-import org.springframework.security.core.Authentication;
-
-public record CurrentUserContext(User user, Authentication auth) {}
