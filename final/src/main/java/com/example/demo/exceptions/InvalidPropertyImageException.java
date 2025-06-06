507e572d30d4227bee1998d46496c98f940d468c
MatthewButtitta <matthewbuttitta@gmail.com>
2025-06-06 17:00:31 -0500

    fixed exception

---
+++ final/src/main/java/com/example/demo/exceptions/InvalidPropertyImageException.java
@@ -0,0 +1,7 @@
+package com.example.demo.exceptions;
+
+public class InvalidPropertyImageException extends RuntimeException {
+    public InvalidPropertyImageException(String message) {
+        super(message);
+    }
+}
